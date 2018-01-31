f4ec5ae8e7dedd8c4590694936b70789356325a2
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2018-01-31 15:10:43 -0800

    modify price prototype method

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,42 @@
+
+// Business logic
+function Pizza(size, toppings){
+  this.size=size;
+  this.toppings=toppings;
+  this.quantityOfToppings = [];
+  this.price;
+}
+
+// Prototype
+pizza.prototype.price=function(){
+  var price = 7;
+  if this.size==="Small" {
+    this.price+=2;
+  } else if (this.size==="Medium"){
+    this.price+=4;
+  } else if(this.size==="Large") {
+      price+=6
+  }
+
+  if (this.quantityOfToppings.length===0{
+    price += 1;
+  }else{
+    price +=this.quantityOfToppings.length;
+  }
+
+  return price;
+};
+
+
+
 // User interface logic
 $(document).ready(function(){
   $("form#yourOptions").submit(function(event){
     event.preventDefault();
 
     var pizzaSize = $("select#pizzaSize").val();
-    var newOrder = new pizza(pizzaSize);
+    var newOrder = new Pizza(pizzaSize);
 
 
   })
 })
-
-// Business logic
-function pizza(size, toppings){
-  this.size=size;
-  this.toppings=toppings;
-  this.quantityOfToppings
-  this.price;
-}
-
-// Prototype
-pizza.prototype.price=function(){
-  var price = 9;
-  if this.size==="Small" {
-    price+=5;
-  } else if (this.size==="Medium"){
-    price+=3;
-  } else {
-    price+=0
-  }
